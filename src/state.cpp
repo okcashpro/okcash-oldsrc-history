a2ffbcb6fa2ba421c9d12b5fb963312edede6ac3
OKtoshi <devteam@okcash.co>
2015-09-22 19:29:46 -0500

    rmvd thin

--- src/state.cpp
+++ src/state.cpp
@@ -12,8 +12,8 @@ int nBloomFilterElements = 1536;
 int nMinStakeInterval = 0;         // in seconds, min time between successful stakes
 int nThinIndexWindow = 4096;        // no. of block headers to keep in memory
 
-// -- services provided by local node, initialise to all on
-uint64_t nLocalServices     = 0 | NODE_NETWORK | THIN_SUPPORT | THIN_STEALTH | SMSG_RELAY;
+// -- services provided by local node, initialise to all on THIN_SUPPORT | THIN_STEALTH | rmvd
+uint64_t nLocalServices     = 0 | NODE_NETWORK | SMSG_RELAY;
 uint32_t nLocalRequirements = 0 | NODE_NETWORK;
 
 
