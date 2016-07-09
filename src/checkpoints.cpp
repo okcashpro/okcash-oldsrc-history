1851609c703a78dc4f2605b8e98f22b718e759c3
Oktoshi <devteam@okcash.co>
2016-07-08 20:39:53 -0500

    security checkp

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -40,6 +40,7 @@ namespace Checkpoints
         ( 500000,   uint256("0x2d2139f39759be2e70b1ea42211ccf07c2a20787f81485b2cc4544166dd57bef") )
         ( 560000,   uint256("0x9bab9b7588d4a2929ec36a27b5c7fccf1ae9063b408d91f92fc288a1c1dea751") )
         ( 720000,   uint256("0xd379d5bba81966d4fe3d7d1065a2e8be2380582d12a92101e4d648d14b32055f") )
+        ( 798000,   uint256("0xb84acefdd7acd8b63e78fab121ed57aa5428e5f634c6f9c09d7cfe79a4ff19fc") )
     ;
 
     // TestNet has no checkpoints
